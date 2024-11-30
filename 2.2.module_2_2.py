591cd6cba2b51f61401353d9cadb1d5c3a7b4900
fontan25 <63672298+fontan25@users.noreply.github.com>
2024-12-01 01:36:14 +0300

    Update 2.2.module_2_2.py

--- 2.2.module_2_2.py
+++ 2.2.module_2_2.py
@@ -3,8 +3,8 @@ second = int(input('Введите второе число: '))
 third = int(input('Введите третье число: '))
 
 if first == second == third:                               # Если все числа равны между собой
-    print('Вывод: 3')
+    print('3')
 elif first == second or first == third or second == third: # Если хотя бы 2 из 3 введённых чисел равны между собой
-    print('Вывод: 2')
+    print('2')
 else:                                                      # Если равных чисел среди 3-х вообще нет
-    print('Вывод: 0')
+    print('0')
