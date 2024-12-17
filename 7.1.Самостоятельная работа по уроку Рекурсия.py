2d42fbb3cd9002f1b7d122edb09455f05401b29d
fontan25 <63672298+fontan25@users.noreply.github.com>
2024-12-17 22:29:39 +0300

    Add files via upload

---
+++ 7.1.Самостоятельная работа по уроку Рекурсия.py
@@ -0,0 +1,12 @@
+def get_multiplied_digits(number):
+    number_str = str(number)               # Преобразуем число в строку
+    product = 1                            # Инициализируем переменную для хранения произведения
+
+    for digit in number_str:               # Проходим по каждой цифре в строке
+        if digit != '0':                   # Если цифра не равна нулю,
+            product *= int(digit)          # умножаем ее на текущее значение произведения
+    return product                         # Возвращаем произведение цифр
+
+# Пример использования функции
+print(get_multiplied_digits(40203))   # Выведет: 24
+print(get_multiplied_digits(402030))  # Выведет: 24
